c6460cc313c4368dcf9a2b7f34e0e7e26fe4f764
RuqayyahSara <sarasadiya4@gmail.com6>
2018-08-09 10:22:23 +0530

    exporting strings,obj,fun....

--- index.js
+++ index.js
@@ -5,12 +5,4 @@ var m1 = require("./functions");
 
     console.log(login);
     console.log(m.msg1);
-    console.log(m1.msg2("hello Sara"));
-
-    db.mycollection.insert(
-        [
-          { _id: 11, item: "pencil", qty: 50, type: "no.2" },
-          { item: "pen", qty: 20 },
-          { item: "eraser", qty: 25 }
-        ]
-     )
+    console.log(m1.msg2("Hello Sara"));
